0fc36455d472ba6aa332807c6fa427a3e3dfc857
Bio97-2001 <cuevasfabio1@gmail.com>
2025-09-25 08:45:31 -0600

    Update Codigo.cs

--- Assets/Scripts/Codigo.cs
+++ Assets/Scripts/Codigo.cs
@@ -5,7 +5,7 @@ public class Codigo : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     print("hola");   
     }
 
     // Update is called once per frame
