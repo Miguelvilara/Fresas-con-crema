1bcb65e45a680f502485f6e6e3af9cc5e16fc5f5
Miguelvilara <miguelvilara64@gmail.com>
2025-10-02 08:13:09 -0600

    enemy movement script listo

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform target;
+    private int pathIndex = 0;
+
+    private void Start()
     {
-        
+        target = LevelManager.main.path[pathIndex];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
+            pathIndex++;
+
+            if (pathIndex == LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = LevelManager.main.path[pathIndex];
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.linearVelocity = direction * moveSpeed;
     }
 }
+
