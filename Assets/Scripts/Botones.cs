69a4a7440c3b8cc5bd20af585c16a0483797dc13
Lonardio <leonardosalrey@gmail.com>
2025-10-07 12:46:57 -0600

    Agregue un boton

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // <- Esta línea es esencial
+
+public class Botones : MonoBehaviour
+
+{
+
+    public void Jugar()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        Debug.Log("Botón presionado, cargando escena...");
+    }
+
+    public void Salir()
+    { }
+
+
+
+
+}
