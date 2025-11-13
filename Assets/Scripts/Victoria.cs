b381e1d8b3ef9a086cd9157159fbbc8fd9472c83
Miguelvilara <miguelvilara64@gmail.com>
2025-11-13 08:31:29 -0600

    armado nivel 2 level manager

--- Assets/Scripts/Victoria.cs
+++ Assets/Scripts/Victoria.cs
@@ -5,10 +5,24 @@ public class Victoria : MonoBehaviour
 {
     public string menuSceneName = "Menu2";
     public string gameSceneName = "Nivel2";
+    private string nombreEscena2 = "Nivel2";
+    private string nombreEscena3 = "Nivel3";
 
     public void Nivel2()
     {
-        SceneManager.LoadScene(gameSceneName);
+        string ultimoNivel = PlayerPrefs.GetString("UltimoNivel", "");
+        if (ultimoNivel.Contains("Nivel1"))
+        {
+            //Esto quiere decir que el jugador viene del nivel 1 y por tanto hay que llevarlo al nivel 2. 
+            SceneManager.LoadScene(nombreEscena2);
+        }
+
+        if (ultimoNivel.Contains("Nivel2"))
+        {
+            SceneManager.LoadScene(nombreEscena3);
+
+        }
+       
     }
 
     public void LoadMainMenu()
