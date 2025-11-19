c4588094408937301d4833d5f72d9d87ca557705
DonRuso <123786290+DonRuso@users.noreply.github.com>
2025-11-19 15:20:52 -0600

    Actualizacion menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 using TMPro;
 
 public class Menu : MonoBehaviour {
- 
+ 
 	[Header("References")]
 	[SerializeField] TextMeshProUGUI currencyUI;
-	[SerializeField] Animator anim;
+	// [SerializeField] Animator anim; // <-- ¡Eliminada!
 
 	private bool isMenuOpen = true;
 	
 	public void ToggleMenu(){
 		isMenuOpen = !isMenuOpen;
-		anim.SetBool("MenuOpen", isMenuOpen);
+		// anim.SetBool("MenuOpen", isMenuOpen); // <-- ¡Eliminada!
 	}
 
 	private void OnGUI() {
